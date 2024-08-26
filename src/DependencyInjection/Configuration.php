501344b4d9ca29f8ff73458a5d4636377fad7a46
Frederik Rommel <frederik.rommel@webidea24.de>
2024-08-26 23:37:59 +0200

    add storefront implementation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,10 +15,29 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('frosh_sentry');
         $rootNode = $treeBuilder->getRootNode();
 
-        $rootNode
+        // @formatter:off
+        $rootNode // @phpstan-ignore-line
             ->children()
                 ->booleanNode('report_scheduled_tasks')->defaultFalse()->end()
+                ->arrayNode('storefront')
+                    ->children()
+                        ->booleanNode('enabled')->defaultFalse()->end()
+                        ->arrayNode('replay_recording')
+                            ->children()
+                                ->booleanNode('enabled')->defaultFalse()->end()
+                                ->floatNode('sample_rate')->defaultValue(0.1)->end()
+                            ->end()
+                        ->end()
+                        ->arrayNode('tracing')
+                            ->children()
+                                ->booleanNode('enabled')->defaultFalse()->end()
+                                ->floatNode('sample_rate')->defaultValue(0.1)->end()
+                            ->end()
+                        ->end()
+                        ->scalarNode('javascript_sdk_version')->defaultValue('8.26.0')->end()
+                    ->end()
             ->end();
+        // @formatter:on
 
         return $treeBuilder;
     }
